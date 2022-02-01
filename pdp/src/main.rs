40a391597e9d32d1e7431f98342cefc091d0618a
Elias Djupesland <elias.djupesland@autostoresystem.com>
2022-02-01 23:23:41 +0100

    learning rust

--- pdp/src/main.rs
+++ pdp/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::process;
+
+use pdp::Config;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("In file {}", config.filename);
+
+    if let Err(e) = pdp::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
