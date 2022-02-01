5545f058bea423924962baf50d1cb6a0c7baf519
Elias Djupesland <elias.djupesland@autostoresystem.com>
2022-02-01 23:04:18 +0100

    update gitignore

--- pdp/target/rls/debug/deps/pdp-fb32d4a9f7df952e.d
+++
@@ -1,5 +0,0 @@
-/home/elias/Projects/INF273/pdp/target/rls/debug/deps/pdp-fb32d4a9f7df952e.rmeta: src/main.rs
-
-/home/elias/Projects/INF273/pdp/target/rls/debug/deps/pdp-fb32d4a9f7df952e.d: src/main.rs
-
-src/main.rs:
